13abb87b6a5cb2730ee07feba4a53b421fc893c5
Gustavo Koch <gustavok@outlook.com.br>
2021-05-28 22:29:26 -0300

    add product update and delete components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { ProductCreateComponent } from './components/product/product-create/product-create.component'
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component'
+
 import { ProductCrudComponent } from './views/product-crud/product-crud.component'
 import { HomeComponent } from './views/home/home.component'
 
@@ -17,6 +20,14 @@ const routes: Routes = [
   {
     path: "products/create",
     component: ProductCreateComponent
+  },
+  {
+    path: "products/update/:id",
+    component: ProductUpdateComponent
+  },
+  {
+    path: "products/delete/:id",
+    component: ProductDeleteComponent
   }
 ]
 
--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -33,7 +33,7 @@ export class ProductService {
     return this.http.get<Product[]>(this.baseUrl)
   }
 
-  readById(id: number): Observable<Product> {
+  readById(id: string | null): Observable<Product> {
     const url = `${this.baseUrl}/${id}`
 
     return this.http.get<Product>(url)
@@ -43,5 +43,11 @@ export class ProductService {
     const url = `${this.baseUrl}/${product.id}`
 
     return this.http.put<Product>(url, product)
+  }
+
+  destroy(id: number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+
+    return this.http.delete<Product>(url)
   }
 }
